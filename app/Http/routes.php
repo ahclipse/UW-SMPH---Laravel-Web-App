b69784fd362ec4621b231f0aa160da4068e89500
Adam <ahclipse@gmail.com>
2015-07-24 14:46:49 -0500

    Initial Commit 'Hello World'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,11 +6,12 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| It's a breeze. Simply tell Laravel the URIs it should respond to    
+| and give it the controller to call when that URI is requested.   
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello World!";
+    //return view('welcome');
 });
